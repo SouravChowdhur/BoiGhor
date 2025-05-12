736eee06b597388cb8b24986adfb7b35a151e254
Sourav Chowdhury <souravchowdhurybnk@gmail.com>
2025-05-12 21:12:47 +0530

    hii

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,12 +1,17 @@
 export const sendToken = (user, statusCode, message, res) => {
-     const token = user.generateToken();
-     res.status(statusCode)
-        .cookie("token", token, {expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-          httpOnly: true})
-        .json({
-             success: true,
-             user,
-             message,
-             token
-        });
+  const token = user.generateToken();
+
+  res.status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: true, // important for HTTPS
+      sameSite: "None" // important for cross-origin cookies (Netlify → Render)
+    })
+    .json({
+      success: true,
+      user,
+      message,
+      token
+    });
 };
