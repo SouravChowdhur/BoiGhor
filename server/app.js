e5a5ad34c5c54fc67e88d96bf8f73443a2a12013
Sourav Chowdhury <souravchowdhurybnk@gmail.com>
2025-05-12 21:01:47 +0530

    hi

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ config({path: "./config/config.env"});
 
 
 app.use(cors({
-    origin: ["https://spontaneous-kataifi-122e24.netlify.app/login"],
+    origin: ["https://spontaneous-kataifi-122e24.netlify.app"],
     METHODS: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
