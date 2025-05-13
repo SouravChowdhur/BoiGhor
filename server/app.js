97ad2d01dfdceb64949dd9f591c8b4d2865b0d03
Sourav Chowdhury <souravchowdhurybnk@gmail.com>
2025-05-13 23:40:31 +0530

    final

--- server/app.js
+++ server/app.js
@@ -18,12 +18,14 @@ export const app = express();
 config({path: "./config/config.env"});
 
 
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }));
 
-app.use(cors({
-    origin: ["https://spontaneous-kataifi-122e24.netlify.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -42,3 +44,4 @@ connectDB();
 notifyUsers();
 
 app.use(errorMiddleware)
+
